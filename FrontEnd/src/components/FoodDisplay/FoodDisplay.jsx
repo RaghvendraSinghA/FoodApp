5a970526887e2f8ac649ec86337082bdc3cfd9cb
“RaghvendraSinghA” <satyamsingh41071@gmail.com>
2025-06-18 20:13:57 +0530

    first commit

---
+++ FrontEnd/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {useContext} from 'react'
+import {StoreContext} from '../../context/StoreContext.jsx'
+import './FoodDisplay.css'
+import FoodItem from '../FoodItem/FoodItem.jsx'
+
+
+const FoodDisplay =({category})=>{
+    const {food_list}=useContext(StoreContext)
+    return(<div className="food-display" id='food-display'>
+        <h2>Available Dishes near you</h2>
+        <div className="food-display-list">
+            {food_list.map((item,index)=>{
+                if(category==='All' || category===item.category){
+                    //render this item if category value is All or item category is equal to category else do nothing ,map will return undefined by itself.
+                return(
+                    <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price}  image={item.image}/>
+                )
+            }
+            })}
+        </div>
+
+    </div>)
+}
+export default FoodDisplay
---
+++ FrontEnd/src/context/StoreContext.jsx
@@ -0,0 +1,69 @@
+import {createContext,useState,useEffect} from 'react'
+import {food_list} from "../assets/assets.js"
+
+export const StoreContext=createContext(null)
+
+const StoreContextProvider=(props)=>{
+    const[cartItems,setCartItems]=useState({})
+    const url='http://localhost:4000'
+    const [token,setToken]=useState("")
+
+    const addToCart=(itemId)=>{
+        //cartItmes is like hashmap and we using itemId as key
+        if(!cartItems[itemId]){
+            setCartItems(prev=>({...prev,[itemId]:1}))
+        }else{
+            setCartItems(prev=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    const removeFromCart=(itemId)=>{
+        setCartItems(prev=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    const getTotalCartAmount=()=>{
+        let total=0
+        for(const item in cartItems){
+            if(cartItems[item]>0){          //this checks if item exist in cartItems bcoz it is like hashmap.we increase decrease the quantity.So item can exist but with 0 quantity.
+                let iteminfo=food_list.find((product)=>product._id===item)
+                total=total+iteminfo.price*cartItems[item]
+            }
+        }
+        return total
+    }
+
+    useEffect(()=>{
+        if(localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"))
+            getCartItems() //It loads data from localStorage and sets in cartItems
+        }
+        
+    },[]) //array was empty and getCart was outside.
+
+    useEffect(()=>{
+        setCartItemsInLocalStorage() //it sets cartItems in localStorage.
+    },[cartItems])
+
+    const setCartItemsInLocalStorage=()=>{
+        localStorage.setItem("cartItems", JSON.stringify(cartItems))
+    }
+
+    const getCartItems=()=>{
+        const response=JSON.parse(localStorage.getItem("cartItems"))
+        setCartItems(response)
+    }
+
+
+
+
+    const contextValue={
+        food_list,cartItems,setCartItems,addToCart,removeFromCart,getTotalCartAmount,url,token,setToken
+    }
+
+    return(
+    <StoreContext.Provider value={contextValue}>
+    {props.children}
+    </StoreContext.Provider>
+    )
+}
+export default StoreContextProvider;
